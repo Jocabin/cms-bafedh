ee959d74a0e15c0b3e02e927f45290fbacf510eb
Jocabin <kobyfar@hotmail.com>
2022-11-30 10:37:37 +0100

    change typos and home page

--- index.php
+++ index.php
@@ -14,22 +14,6 @@
 
 get_header();
 ?>
-    <main id="primary" class="site-main">
-        <h1>titre</h1>
-        <p>baseline</p>
 
-        <section class="contact-section">
-            <img width="437" height="244" src="" alt="">
-            <div>
-                <h2>Me contacter</h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad cumque dolore dolorem eos est eveniet
-                    inventore pariatur, porro quas quidem recusandae reiciendis rem repellendus tempore vero vitae
-                    voluptatum. Accusamus, nostrum!
-                </p>
-                <a href="mailto:bastiendb@gmail.com" class="primary-button">Me contacter</a>
-            </div>
-        </section>
-    </main>
 <?php
 get_footer();
