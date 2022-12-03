dba8fe75ce00f3aa5e3297870be4e9dadeb8f383
Jocabin <kobyfar@hotmail.com>
2022-12-03 19:33:07 +0100

    fix product link

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -2,6 +2,9 @@
 defined('ABSPATH') || exit;
 
 get_header();
+global $product;
+
+$link = apply_filters('woocommerce_loop_product_link', get_the_permalink(), $product);
 ?>
     <h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
 
@@ -22,13 +25,13 @@ if (woocommerce_product_loop()) {
             do_action('woocommerce_shop_loop');
             ?>
             <li class="product-tile" tabindex="0">
-                <?php do_action('woocommerce_before_shop_loop_item'); ?>
-                <div>
-                    <?php do_action('woocommerce_before_shop_loop_item_title'); ?>
-                </div>
-                <?php do_action('woocommerce_shop_loop_item_title'); ?>
-                <p>À partir de <?php do_action('woocommerce_after_shop_loop_item_title'); ?></p>
-                <?php do_action('woocommerce_after_shop_loop_item'); ?>
+                <a href="<?php echo esc_url($link) ?>">
+                    <div>
+                        <?php do_action('woocommerce_before_shop_loop_item_title'); ?>
+                    </div>
+                    <?php do_action('woocommerce_shop_loop_item_title'); ?>
+                    <p>À partir de <?php do_action('woocommerce_after_shop_loop_item_title'); ?></p>
+                </a>
             </li>
             <?php
         }
